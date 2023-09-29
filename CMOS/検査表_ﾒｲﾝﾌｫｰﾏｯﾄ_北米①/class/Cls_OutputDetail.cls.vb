28841c9171f028a97dfd44811dcb3f76307e22cb
tool <tool@example.com>
2023-09-29 15:37:08 +0900

    test

---
+++ CMOS/検査表_ﾒｲﾝﾌｫｰﾏｯﾄ_北米①/class/Cls_OutputDetail.cls.vb
@@ -0,0 +1,32 @@
+'測定項目名
+Public ItemName As String
+Public ItemNameLfCnt As Long
+'SPC
+Public SpcValue As String
+'Spec Left/Center/Right
+Public SpecL As String
+Public SpecC As String
+Public SpecR As String
+'単位
+Public Unit As String
+'最大値
+Public MaxN As String
+Public MaxCenterd As Boolean
+'最小値
+Public MinN As String
+Public MinCenterd As Boolean
+'平均値
+Public Ave As String
+Public AveCenterd As Boolean
+'標準偏差
+Public Sigma As String
+Public SigmaCenterd As Boolean
+'cpk
+Public Cpk As String
+Public CpkCenterd As Boolean
+'抜取数(不合格数)+"/"+抜取数(母数)
+Public RN As String
+'判定
+Public result As String
+'画像検査
+Public AviApplied As String
---
+++ CMOS/検査表_ﾒｲﾝﾌｫｰﾏｯﾄ_北米①/class/Cls_OutputHeader.cls.vb
@@ -0,0 +1,11 @@
+'顧客名
+Public Customer As String
+'品名
+Public Article1 As String
+Public Article2 As String
+'出荷数
+Public InspectionQuantity As String
+'出荷ﾛｯﾄNo
+Public LotNo As String
+'出荷日
+Public InspectionDate As String
